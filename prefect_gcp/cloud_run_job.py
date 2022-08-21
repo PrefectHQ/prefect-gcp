f0c75dfda7e9e05c7092039598f608cc70c5084c
peytonrunyan <peytonrunyan@gmail.com>
2022-08-21 14:33:37 -0400

    submitting a run works

--- prefect_gcp/cloud_run_job.py
+++ prefect_gcp/cloud_run_job.py
@@ -1,57 +1,88 @@
+from typing import Any, Optional
 from prefect.infrastructure.base import Infrastructure, InfrastructureResult
-
+from google.api_core.client_options import ClientOptions
 from googleapiclient import discovery
 from google.oauth2 import service_account
-
+from prefect.blocks.core import Block
 from prefect_gcp.credentials import GcpCredentials
 
-scopes = ['https://www.googleapis.com/auth/cloud-platform']
-sa_file = 'creds.json'
-zone = 'us-central1-a'
-project_id = 'prefect-dev-cloud2' # Project ID, not Project Name
+# scopes = ['https://www.googleapis.com/auth/cloud-platform']
+# sa_file = 'creds.json'
+# region = 'us-east1'
+# project_id = 'helical-bongo-360018' # Project ID, not Project Name
 
-credentials = service_account.Credentials.from_service_account_file(sa_file, scopes=scopes)
+# api_endpoint = f"https://{region}-run.googleapis.com"
+# options = ClientOptions(
+#     api_endpoint=api_endpoint
+# )
 
-# Create the Cloud Compute Engine service object
-service = discovery.build('compute', 'v1', credentials=credentials)
-with discovery.build('run', 'v1') as client:
-    # request = client.namespaces().jobs().list(parent=f'namespaces/prefect-dev-cloud2')
-    request = client.namespaces().jobs().run(name='namespaces/prefect-dev-cloud2/jobs/peyton-test')
-    response = request.execute()
-    print(response)
+# credentials = service_account.Credentials.from_service_account_file(sa_file, scopes=scopes)
 
+# # Create the Cloud Compute Engine service object
+# service = discovery.build('run', 'v1', client_options=options, credentials=credentials)
+# breakpoint()
+# request = service.namespaces().jobs().list(parent=f'namespaces/helical-bongo-360018')
+# response = request.execute()
+# breakpoint()
+# request = service.namespaces().jobs().run(name='namespaces/helical-bongo-360018/jobs/peyton-test')
+# response = request.execute()
+# print(response)
+# breakpoint()
+class CloudRunJobSetting(Block):
+    pass
 class CloudRunJob(Infrastructure):
     job_name: str
+    type: str = "Cloud Run Job"
     project_id: str
-    zone: str
+    region: str
     image_url: str
     credentials: GcpCredentials
+    registry: Optional[Any]
 
     def run(self):
         with self._get_client() as jobs_client:
-            self._register(jobs_client=jobs_client)
-            self._submit_job_run(jobs_client=jobs_client)
-
+            # self._register(jobs_client=jobs_client)
+            res = self._submit_job_run(jobs_client=jobs_client)
+            print(res)
 
     def _register(self, jobs_client):
         request = jobs_client.create(...)
         # gcloud beta run jobs create <job_name> --image <self.image_url> --project <self.project>
 
     def _submit_job_run(self, jobs_client):
-        request = jobs_client.run(...)
+        request = jobs_client.run(
+            name=f"namespaces/{self.project_id}/jobs/{self.job_name}"
+        )
+        response = request.execute()
+        return response
 
     def _get_client(self):
+        # region needed for 'v1' API
+        api_endpoint = f"https://{self.region}-run.googleapis.com"
         credentials = self.credentials.get_credentials_from_service_account()
-        return discovery.build('run', 'v1', credentials=credentials).namespaces().jobs()
+        options = ClientOptions(api_endpoint=api_endpoint)
 
+        return discovery.build(
+            'run', 
+            'v1',
+            client_options=options,
+            credentials=credentials
+            ).namespaces().jobs()
+
+    def preview(self):
+        pass
 if __name__ == "__main__":
     creds = GcpCredentials(service_account_file="creds.json")
 
     job = CloudRunJob(
         job_name="peyton-test",
-        project_id="prefect-dev-cloud2",
-        zone="us-east1",
-        image_url="gcr.io/prefect-dev-cloud2/peytons-test-image:latest"
+        project_id="helical-bongo-360018",
+        region="us-east1",
+        image_url="gcr.io/prefect-dev-cloud2/peytons-test-image:latest",
+        credentials=creds,
+
     )
+
+    job.run()
 
 
