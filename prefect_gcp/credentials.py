c0b1ad132e2c4e89efc81c50e4200317e02a9065
Andrew <15331990+ahuang11@users.noreply.github.com>
2022-08-26 11:06:32 -0700

    Simplify logic

--- prefect_gcp/credentials.py
+++ prefect_gcp/credentials.py
@@ -101,12 +101,9 @@ class GcpCredentials(Block):
                 "can be specified at once"
             )
         elif service_account_file:
-            if not os.path.exists(os.path.expanduser(service_account_file)):
+            service_account_file = Path(service_account_file).expanduser()
+            if not service_account_file.exists():
                 raise ValueError("The provided path to the service account is invalid")
-            elif isinstance(service_account_file, Path):
-                service_account_file = service_account_file.expanduser()
-            else:
-                service_account_file = os.path.expanduser(service_account_file)
             credentials = Credentials.from_service_account_file(service_account_file)
         elif service_account_info:
             credentials = Credentials.from_service_account_info(service_account_info)
