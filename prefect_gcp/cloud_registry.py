ab75cefc728347506a444004fb08a68b616f6884
peytonrunyan <peytonrunyan@gmail.com>
2022-08-29 13:17:52 -0400

    Cloud registry up to snuff mostly

--- prefect_gcp/cloud_registry.py
+++ prefect_gcp/cloud_registry.py
@@ -12,7 +12,6 @@ if TYPE_CHECKING:
 
     from prefect_gcp.credentials import GcpCredentials
 
-PREFECT_DOCKERHUB_NAME = get_prefect_image_name()
 
 class LocationType(Enum):
     REGION = "Region"
@@ -21,22 +20,17 @@ class LocationType(Enum):
 
 class GoogleCloudRegistry(BaseDockerLogin):
     _block_type_name = "Google Cloud Registry"
-    # registry_url: str = Field(
-    #     ...,
-    #     description='The URL to the registry. Generally, "http" or "https" can be omitted.',
-    # )
     credentials: GcpCredentials
     reauth: bool = Field(
         True, description="Whether or not to reauthenticate on each interaction."
     )
     _registry_name: str = None
 
-    def login(self):
-        client = self._get_docker_client()
-        res = self._login(username=self.username, password=self.password, client=client)
-
     @property 
     def password(self):
+        """
+        Returns the JSON key value from a JSON service account file.
+        """
         service_account_value = self.credentials.get_service_account_value()
         if isinstance(service_account_value, dict):
             password = json.dumps(service_account_value)
@@ -47,29 +41,72 @@ class GoogleCloudRegistry(BaseDockerLogin):
 
     @property
     def username(self):
-        return "_json_key"
+        """Expected username when using the JSON credential value as a password"""
+        return "_json_key" 
+
+    @property
+    def registry_name(self):
+        """The name of the Google Cloud Registry"""
+        if self._registry_name is None:
+            self._registry_name = f"{self.credentials.get_project_id()}" 
+
+        return self._registry_name
+
+    @property
+    def prefect_gcr_name(self):
+        return f"{self.registry_name}/{get_prefect_image_name()}"
+
+    def login(self):
+        client = self._get_docker_client()
+        return self._login(username=self.username, password=self.password, client=client)
+
+    def _login(self, username, password, client):
+        return client.login(
+            username=username,
+            password=password,
+            registry=f"gcr.io/{self.registry_name}",
+            reauth=True,
+        )
 
     def get_prefect_image(self):
+        """Return the name of the Prefect image in GCR.
+        
+        If the image does not exist in GCR, this will add the image to GCR and
+        return the name of the newly pushed image. 
+        """
         self.login()
         client = self._get_docker_client()
-        prefect_image_tag = self._get_tag_for_prefect_image_in_google_registry()
-        if prefect_image_tag is None:
-            prefect_image_tag = self._add_prefect_image_to_registry(client=client)
-        
-        return prefect_image_tag
+        prefect_repository, tag = self.prefect_gcr_name.split(":")
+        repos = self._list_repositories()
+
+        # See if an existing match is up there
+        if prefect_repository in repos:
+            if tag in self._list_tags_for_repository(prefect_repository):
+                return self.prefect_gcr_name
+
+        # Add if existing match not there
+        self._add_prefect_image_to_registry(client=client)
+        return self.prefect_gcr_name
 
     def _add_prefect_image_to_registry(self, client):
-        local_prefect_image = self._get_local_prefect_image()
-        if not local_prefect_image:
+        """Add the correct Prefect docker image to a Google Container Registry.
+
+        This will pull the image from Dockerhub if it does not already exist locally,
+        and then push the image Google Container Registry. Directly adding to GCR is
+        not an option.
+        """
+        prefect_image = self._get_local_prefect_image()
+        if prefect_image is None:
             try:
-                client.images.pull(PREFECT_DOCKERHUB_NAME)
-                local_prefect_image = self._get_local_prefect_image()
+                prefect_image = client.images.pull(get_prefect_image_name())
             except Exception as exc:
                 raise
         
-        local_prefect_image.tag(self.prefect_gcr_name)
+        destination_tag = f"gcr.io/{self.prefect_gcr_name}" 
+
+        prefect_image.tag(destination_tag)
         for line in client.images.push(
-            repository=self.prefect_gcr_name, 
+            repository=destination_tag, 
             stream=True, 
             auth_config={"username": self.username, "password": self.password},
             decode=True
@@ -78,54 +115,41 @@ class GoogleCloudRegistry(BaseDockerLogin):
 
         return self.prefect_gcr_name
 
+    def _list_local_images(self):
+        """List all images on the local machine."""
+        client = self._get_docker_client()
+        images = client.images.list()
 
-    def _get_local_prefect_image(self):
-        for image in self.list_repositories():
-            for tag in image.tags:
-                if tag == PREFECT_DOCKERHUB_NAME:
-                    return image
+        return images
 
-    def _login(self, username, password, client):
-        return client.login(
-            username=username,
-            password=password,
-            registry=self.registry_name,
-            reauth=True,
-        )
-
-    @property
-    def registry_name(self):
-        if self._registry_name is None:
-            self._registry_name = f"gcr.io/{self.credentials.get_project_id()}" 
-
-        return self._registry_name
-
-    @property
-    def prefect_gcr_name(self):
-        return f"{self.registry_name}/{PREFECT_DOCKERHUB_NAME}"
-
-    def list_repositories(self):
+    def _list_repositories(self):
+        """List all GCR repositories associated with the credential."""
         access_token = self.credentials.get_access_token()
         resp = requests.get('https://gcr.io/v2/_catalog', auth=('_token', access_token))
-        breakpoint()
-        return resp.json()
+        return resp.json()["repositories"]
     
     def _list_tags_for_repository(self, repository):
-        prefect_repository_name = get_prefect_image_name.split(":")[0]
+        """List all tags for a given repository."""
         access_token = self.credentials.get_access_token()
-        
         resp = requests.get(
-            f"https:{self.registry_name}/{prefect_repository_name}/tags/list", 
+            f"https://gcr.io/v2/{repository}/tags/list", 
             auth=('_token', access_token)
         )
-        return resp.json()
+        tags = resp.json()["tags"] 
+        if tags == []:
+            raise Exception(f"No repository matching name {repository} was found")
+        return tags 
 
-    def _get_tag_for_prefect_image_in_google_registry(self):
-        for image in self.list_repositories():
-            breakpoint()
-            for tag in image.tags:
-                if self.prefect_gcr_name in tag:
-                    return tag
+    def _get_local_prefect_image(self):
+        """Get an image object for the expected Prefect image."""
+        client = self._get_docker_client()
+        if get_prefect_image_name() not in self._list_local_images():
+            try:
+                local_prefect_image = client.images.pull(get_prefect_image_name())
+                return local_prefect_image
+            except Exception as exc:
+                raise 
+        return None
 
 
 if __name__ == "__main__":
@@ -133,8 +157,8 @@ if __name__ == "__main__":
     registry = GoogleCloudRegistry(
         credentials=creds,
     )
-    registry.login()
-    print(registry.list_repositories())
+
+    registry.get_prefect_image()
 
 # class ContainerRegistry(GoogleCloudRegistry):
 #     pass
