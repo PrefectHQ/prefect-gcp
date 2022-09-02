1433f9a52ff15e9780607a70e85e6b94fa24ba4e
peytonrunyan <peytonrunyan@gmail.com>
2022-09-02 13:55:15 -0400

    add tests for jobs

--- tests/test_cloud_run_job.py
+++ tests/test_cloud_run_job.py
@@ -1,25 +1,150 @@
+from importlib.metadata import metadata
 import pytest
-from prefect_gcp.cloud_run_job import CloudRunJob, Execution
+from prefect_gcp.cloud_run_job import CloudRunJob, Execution, Job
 from prefect_gcp.credentials import GcpCredentials
 from prefect.settings import temporary_settings, PREFECT_API_URL, PREFECT_API_KEY, PREFECT_PROFILES_PATH
 from googleapiclient.http import HttpMock
 from googleapiclient import discovery
+from unittest.mock import Mock
+
 class TestJob:
-    def test_get_execution_status(self):
-        pass
+    @pytest.mark.parametrize(
+        "ready_condition,expected_value",
+        [
+            ({"status": "True"}, True),
+            ({"status": "False"}, False),
+            ({}, False)
+        ]
+    )
+    def test_is_ready(self, ready_condition, expected_value):
+        job = Job(
+            metadata={}, 
+            spec={}, 
+            status={}, 
+            name="test", 
+            ready_condition=ready_condition,
+            execution_status={}
+            )
+        assert job.is_ready() == expected_value
 
-    def test_get_ready_condition(self):
-        pass
+    @pytest.mark.parametrize(
+        "status,expected_value",
+        [
+            ({}, {}),
+            ({"conditions": []}, {}),
+            ({"conditions": [{"type": "Dog", "val": "value"}]}, {}),
+            ({"conditions": [{"type": "Ready", "val": "value"}]}, {"type": "Ready", "val": "value"}),
+            ({"conditions": [
+                {"type": "Dog", "val": "value"},
+                {"type": "Ready", "val": "value"}
+                ]
+            }, 
+                {"type": "Ready", "val": "value"})
+        ]
+    )
+    def test_get_ready_condition(self, status, expected_value):
+        assert Job._get_ready_condition({"status": status}) == expected_value
 
-    def test_is_ready(self):
-        pass
+    @pytest.mark.parametrize(
+        "status,expected_value",
+        [
+            ({}, {}),
+            ({"latestCreatedExecution": {}}, {}),
+            ({"latestCreatedExecution": {"some": "val"}}, {"some": "val"}),
+        ]
+    )
+    def test_get_execution_status(self, status, expected_value):
+        assert Job._get_execution_status({"status": status}) == expected_value
 
-    def test_is_finished(self):
-        pass
+    @pytest.mark.parametrize(
+        "execution_status,expected_value",
+        [
+            ({}, True), # Has no execution
+            ({"completionTimestamp": None}, True), # Has an execution with no completion timestamp
+            ({"completionTimestamp": "Exists"}, False), # Has an execution and it has a completion timestamp
+        ]
+    )
+    def test_has_execution_in_progress(self, execution_status, expected_value):
+        job = Job(
+            metadata={}, 
+            spec={}, 
+            status={}, 
+            name="test", 
+            ready_condition={},
+            execution_status=execution_status
+            )
+        assert job.has_execution_in_progress() == expected_value
 
-    def test_from_json(self):
-        pass
-    
+    @pytest.mark.parametrize(
+        "condition,execution,expected_condition,expected_ex_status",
+        [
+            (   # Nothing -> nothing
+                None,
+                None,
+                {},
+                {}
+            ),
+            (   # Nothing -> nothing
+                {},
+                {},
+                {},
+                {}
+            ),
+            (   # Empty conditions
+                {"conditions": []},
+                {},
+                {},
+                {}
+            ),
+            (   # Ready status -> ready status
+                {"conditions": [{"type": "Ready", "dog": "cat"}]},
+                {},
+                {"type": "Ready", "dog": "cat"},
+                {}
+            ),
+            (   # Ready status and execution -> ready status and execution
+                {"conditions":[{"type": "Ready", "dog": "cat"}]},
+                {"latestCreatedExecution": {"puppy": "kitty"}},
+                {"type": "Ready", "dog": "cat"},
+                {"puppy": "kitty"}
+            ),
+            (   # Other status and execution -> nothing and execution
+                {"conditions": [{"type": "OtherThing", "dog": "cat"}]},
+                {"latestCreatedExecution": {"puppy": "kitty"}},
+                {},
+                {"puppy": "kitty"}
+            ),
+            (   # multiple status items -> ready status
+                {"conditions":[
+                    {"type": "OtherThing", "dog": "cat"},
+                    {"type": "Ready", "dog": "cat"}
+                    ]},
+                {"latestCreatedExecution": {"puppy": "kitty"}},
+                {"type": "Ready", "dog": "cat"},
+                {"puppy": "kitty"}
+            ),
+        ]
+    )
+    def test_from_json(self, condition, execution, expected_condition, expected_ex_status):
+        status = {}
+        if condition is not None:
+            status = {**status, **condition}
+        if execution is not None:
+            status = {**status, **execution}
+
+        job_dict = {
+            "metadata": {"name": "Test"},
+            "spec": {"MySpec": "spec"},
+            "status": status        
+        }
+        job = Job.from_json(job_dict)
+
+        assert job.metadata == job_dict["metadata"]
+        assert job.spec == job_dict["spec"]
+        assert job.name == job_dict["metadata"]["name"]
+        assert job.status == job_dict["status"]
+        assert job.ready_condition == expected_condition
+        assert job.execution_status == expected_ex_status
 
 class TestExecution:
     def test_succeeded_responds_true(self):
@@ -200,6 +325,9 @@ def test_mock_client():
     res = HttpMock('tests/api_test_files/dog.json', {'status': 200})
     api_key = "test"
     service = discovery.build("run", "v1", http=res)
+
 class TestCloudRunJobGCPInteraction:
+
+
     def test_create(self):
-        pass
+        mock_client = Mock()
