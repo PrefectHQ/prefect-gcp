31eb536340ed4b6e31239e46672c225fa06617a6
ahuang11 <andrew.h@prefect.io>
2022-11-30 18:38:04 -0800

    Add working code

--- prefect_gcp/aiplatform.py
+++ prefect_gcp/aiplatform.py
@@ -1,16 +1,21 @@
 import datetime
-from typing import Dict, Optional
+import time
+from typing import Dict, Optional, Tuple
 from uuid import uuid4
 
 from anyio.abc import TaskStatus
 from google.api_core.client_options import ClientOptions
+from google.cloud.aiplatform.gapic import JobServiceClient
 from google.cloud.aiplatform_v1.types.custom_job import (
+    ContainerSpec,
     CustomJob,
     CustomJobSpec,
     Scheduling,
     WorkerPoolSpec,
 )
+from google.cloud.aiplatform_v1.types.job_state import JobState
 from google.cloud.aiplatform_v1.types.machine_resources import MachineSpec
+from google.protobuf.duration_pb2 import Duration
 from prefect.infrastructure import Infrastructure, InfrastructureResult
 from prefect.utilities.asyncutils import run_sync_in_worker_thread, sync_compatible
 from pydantic import Field
@@ -62,16 +67,16 @@ class VertexAICustomTrainingJob(Infrastructure):
         title="Environment Variables",
         description="Environment variables to be passed to your Cloud Run Job.",
     )
-    machine_type: Optional[str] = Field(
-        default=None,
+    machine_type: str = Field(
+        default="n1-standard-4",
         description="The machine type to use for the run, which controls the available "
         "CPU and memory.",
     )
     accelerator_type: Optional[str] = Field(
         default=None, description="The type of accelerator to attach to the machine."
     )
-    maximum_run_time: Optional[datetime.timedelta] = Field(
-        default=None, description="The maximum job running time. The default is 7 days."
+    maximum_run_time: datetime.timedelta = Field(
+        default=datetime.timedelta(days=7), description="The maximum job running time."
     )
     network: Optional[str] = Field(
         default=None,
@@ -89,6 +94,14 @@ class VertexAICustomTrainingJob(Infrastructure):
             "Platform Custom Code Service Agent for the CustomJob's project is "
             "used. Takes precedence over the service account found in gcp_credentials, "
             "and required if a service account cannot be detected in gcp_credentials."
+        ),
+    )
+
+    job_watch_poll_interval: float = Field(
+        default=5.0,
+        description=(
+            "The amount of time to wait between GCP API calls while monitoring the "
+            "state of a Vertex AI Job."
         ),
     )
 
@@ -118,6 +131,102 @@ class VertexAICustomTrainingJob(Infrastructure):
     def preview(self) -> str:
         return "not implemented..."
 
+    def _build_job_spec(self) -> CustomJobSpec:
+        # gather worker pool spec
+        env_list = [{"name": name, "value": value} for name, value in self.env.items()]
+        container_spec = ContainerSpec(
+            image_uri=self.image, command=self.command, args=[], env=env_list
+        )
+        machine_spec = MachineSpec(
+            machine_type=self.machine_type, accelerator_type=self.accelerator_type
+        )
+        worker_pool_spec = WorkerPoolSpec(
+            container_spec=container_spec, machine_spec=machine_spec, replica_count=1
+        )
+
+        # look for service account
+        service_account = (
+            self.service_account or self.gcp_credentials._service_account_email
+        )
+        if service_account is None:
+            raise ValueError(
+                "Could not detect a service_account through gcp_credentials; "
+                "please provide a service_account in VertexAICustomTrainingJob"
+            )
+
+        # build custom job specs
+        timeout = Duration().FromTimedelta(td=self.maximum_run_time)
+        scheduling = Scheduling(timeout=timeout)
+        job_spec = CustomJobSpec(
+            worker_pool_specs=[worker_pool_spec],
+            service_account=service_account,
+            scheduling=scheduling,
+            network=self.network,
+        )
+        return job_spec
+
+    async def _create_and_begin_job(
+        self, job_spec: CustomJobSpec, job_service_client: JobServiceClient
+    ) -> CustomJob:
+        # create custom job
+        project = self.gcp_credentials.project
+        resource_name = f"projects/{project}/locations/{self.region}"
+        custom_job = CustomJob(display_name=self.job_name, job_spec=job_spec)
+
+        # run job
+        self.logger.info(
+            f"{self._log_prefix}: Preparing to run command {' '.join(self.command)!r} "
+            f"in region {self.region!r} using image {self.image!r}..."
+        )
+
+        custom_job_run = await run_sync_in_worker_thread(
+            job_service_client.create_custom_job,
+            parent=resource_name,
+            custom_job=custom_job,
+        )
+        return custom_job_run
+
+    async def _watch_job_run(
+        self,
+        full_job_name: str,  # different from self.job_name
+        job_service_client: JobServiceClient,
+        current_state: JobState,
+        until_states: Tuple[JobState],
+        timeout: int = None,
+    ) -> CustomJob:
+        last_state = state = current_state
+        t0 = time.time()
+
+        while state not in until_states:
+            job_run = await run_sync_in_worker_thread(
+                job_service_client.get_custom_job,
+                name=full_job_name,
+            )
+            state = job_run.state
+            if state != last_state:
+                state_label = (
+                    state.name.replace("_", " ")
+                    .lower()
+                    .replace("state", "state is now:")
+                )
+                # results in "New job state is now: succeeded"
+                self.logger.info(f"{self._log_prefix}: New {state_label}")
+                last_state = state
+            else:
+                # Intermittently, the job will not be described. We want to respect the
+                # watch timeout though.
+                self.logger.debug(f"{self._log_prefix}: Job not found.")
+
+            elapsed_time = time.time() - t0
+            if timeout is not None and elapsed_time > timeout:
+                raise RuntimeError(
+                    f"Timed out after {elapsed_time}s while watching job for states "
+                    "{until_states!r}"
+                )
+            time.sleep(self.job_watch_poll_interval)
+
+        return job_run
+
     @sync_compatible
     async def run(
         self, task_status: Optional[TaskStatus] = None
@@ -131,57 +240,38 @@ class VertexAICustomTrainingJob(Infrastructure):
         Returns:
             The `VertexAICustomTrainingJobResult`.
         """
-        # instantiate client
         client_options = ClientOptions(
             api_endpoint=f"{self.region}-aiplatform.googleapis.com"
         )
-        job_service_client = self.gcp_credentials.get_job_service_client(
+        with self.gcp_credentials.get_job_service_client(
             client_options=client_options
-        )
+        ) as job_service_client:
+            job_spec = self._build_job_spec()
+            job_run = await self._create_and_begin_job(job_spec, job_service_client)
 
-        # gather worker pool spec
-        machine_spec = MachineSpec(
-            machine_type=self.machine_type, accelerator_type=self.accelerator_type
-        )
-        worker_pool_spec = WorkerPoolSpec(machine_spec=machine_spec, replica_count=1)
+            if task_status:
+                task_status.started(self.job_name)
 
-        # look for service account
-        service_account = (
-            self.service_account or self.gcp_credentials._service_account_email
-        )
-        if service_account is None:
-            raise ValueError(
-                "Could not detect a service_account through gcp_credentials; "
-                "please provide a service_account in VertexAICustomTrainingJob"
+            final_job_run = await self._watch_job_run(
+                full_job_name=job_run.name,
+                job_service_client=job_service_client,
+                current_state=job_run.state,
+                until_states=(
+                    JobState.JOB_STATE_SUCCEEDED,
+                    JobState.JOB_STATE_FAILED,
+                    JobState.JOB_STATE_CANCELLED,
+                    JobState.JOB_STATE_EXPIRED,
+                ),
+                timeout=self.maximum_run_time.total_seconds(),
             )
 
-        # build custom job specs
-        scheduling = Scheduling(timeout=self.maximum_run_time)
-        job_spec = CustomJobSpec(
-            worker_pool_specs=[worker_pool_spec],
-            service_account=service_account,
-            scheduling=scheduling,
-            network=self.network,
-        )
+        error_msg = final_job_run.error.message
+        if error_msg:
+            raise RuntimeError(f"{self._log_prefix}: {error_msg}")
 
-        # create custom job
-        project = self.gcp_credentials.project
-        resource_name = f"projects/{project}/locations/{self.region}"
-        custom_job = CustomJob(display_name=self.job_name, job_spec=job_spec)
-
-        # run job
-        self.logger.info(
-            f"{self._log_prefix}: Preparing to run command {' '.join(self.command)!r} "
-            f"in region {self.region!r} using image {self.image!r}..."
-        )
-        response = await run_sync_in_worker_thread(
-            job_service_client.create_custom_job,
-            parent=resource_name,
-            custom_job=custom_job,
-        )
-
+        status_code = final_job_run.state.value
         return VertexAICustomTrainingJobResult(
-            identifier=response.name, status_code=response.state.value
+            identifier=final_job_run.display_name, status_code=status_code
         )
 
     @property
