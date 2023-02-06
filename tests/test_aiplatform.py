7dbfe2fb90ab30298c4a8cf404eb01f3d48ac22e
Salman Mohammed <smohammed1993@gmail.com>
2023-02-05 19:25:05 -0500

    Add base env variables to pass settings from agent to Vertex job, & fix test_preview

--- tests/test_aiplatform.py
+++ tests/test_aiplatform.py
@@ -23,29 +23,60 @@ class TestVertexAICustomTrainingJob:
     # TODO: Improve test resiliency to changes in str output
     def test_preview(self, vertex_ai_custom_training_job: VertexAICustomTrainingJob):
         actual_lines = vertex_ai_custom_training_job.preview().splitlines()
-        expected_lines = """
+
+        base_env = VertexAICustomTrainingJob._base_environment().copy()
+        env_lines = [
+            f'env {{\n        name: "{name}"\n        value: "{value}"\n      }}'
+            for name, value in base_env.items()
+        ]
+        env_str = "\n".join(env_lines)
+
+        expected_lines = f"""
             display_name: "container
-            job_spec {
-                worker_pool_specs {
-                    container_spec {
+            job_spec {{
+                worker_pool_specs {{
+                    container_spec {{
                         image_uri: "us-docker.pkg.dev/cloudrun/container/job:latest"
                         command: "echo"
                         command: "hello!!"
-                    }
-                    machine_spec {
+                        {env_str}
+                    }}
+                    machine_spec {{
                         machine_type: "n1-standard-4"
-                    }
+                    }}
                     replica_count: 1
-                }
-                scheduling {
-                }
+                }}
+                scheduling {{
+                }}
                 service_account: "my_service_account_email"
-            }
+            }}
         """.strip().splitlines()
+
         for actual_line, expected_line in zip(actual_lines, expected_lines):
             if '"container' in actual_line:
                 actual_line = actual_line.split("-")[0]  # remove the unique hex
             assert actual_line.strip() == expected_line.strip()  # disregard whitespace
+
+    def test_environment_variables(self, gcp_credentials):
+        vertex_job = VertexAICustomTrainingJob(
+            command=["echo", "hello!!"],
+            region="us-east1",
+            image="us-docker.pkg.dev/cloudrun/container/job:latest",
+            gcp_credentials=gcp_credentials,
+            env={"FOO": "BAR"},
+        )
+        job_spec = vertex_job._build_job_spec()
+
+        assert len(job_spec.worker_pool_specs) == 1
+
+        env_list_in_container_spec = job_spec.worker_pool_specs[0].container_spec.env
+
+        expected_env = VertexAICustomTrainingJob._base_environment().copy()
+        expected_env.update({"FOO": "BAR"})
+
+        for item in env_list_in_container_spec:
+            assert item.name in expected_env
+            assert item.value == expected_env[item.name]
 
     def test_kill(
         self, vertex_ai_custom_training_job: VertexAICustomTrainingJob, caplog
