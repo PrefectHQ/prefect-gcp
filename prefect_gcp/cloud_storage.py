15b4fd9dddaad59f67a5f5a172d139c94fe73bf4
Alexander Streed <desertaxle@users.noreply.github.com>
2023-02-07 16:27:08 -0600

    Update prefect_gcp/cloud_storage.py
    Co-authored-by: nate nowack <nate@prefect.io>

--- prefect_gcp/cloud_storage.py
+++ prefect_gcp/cloud_storage.py
@@ -472,7 +472,7 @@ class GcsBucket(WritableDeploymentStorage, WritableFileSystem, ObjectStorageBloc
     """
     Block used to store data using GCP Cloud Storage Buckets.
 
-    Note! `GcsBucket` in `prefect-gcp` is a unique block separate from `GCS`
+    Note! `GcsBucket` in `prefect-gcp` is a unique block, separate from `GCS`
     in core Prefect. `GcsBucket` does not use `gcsfs` under the hood,
     instead using the `google-cloud-storage` package, and offers more configuration
     and functionality.
