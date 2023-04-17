0f38c05976620f4358b51914c41aec76007fafc9
jake@prefect.io <jake@prefect.io>
2023-04-17 09:20:43 -0400

    black, remove older stuff

--- prefect_gcp/worker.py
+++ prefect_gcp/worker.py
@@ -1,26 +1,26 @@
-
-from typing import Any, Dict, List, Optional, Literal
-from pydantic import Field
-from typing import Any, Dict, List, Optional
-from uuid import uuid4
 import re
 import time
-from prefect.workers.base import BaseWorker, BaseJobConfiguration, BaseVariables, BaseWorkerResult
+from typing import Any, Dict, List, Optional
+from uuid import uuid4
+
+import anyio
 import googleapiclient
 from anyio.abc import TaskStatus
 from google.api_core.client_options import ClientOptions
 from googleapiclient import discovery
 from googleapiclient.discovery import Resource
-from prefect.exceptions import InfrastructureNotFound
-from prefect.infrastructure.base import Infrastructure, InfrastructureResult
-from prefect.utilities.asyncutils import run_sync_in_worker_thread, sync_compatible
-from pydantic import BaseModel, Field, root_validator, validator
-from typing import Dict, Any
-import anyio
-from prefect_gcp.credentials import GcpCredentials
-
-from prefect_gcp.cloud_run import Job, Execution
 from prefect.docker import get_prefect_image_name
+from prefect.utilities.asyncutils import run_sync_in_worker_thread, sync_compatible
+from prefect.workers.base import (
+    BaseJobConfiguration,
+    BaseVariables,
+    BaseWorker,
+    BaseWorkerResult,
+)
+from pydantic import Field
+
+from prefect_gcp.cloud_run import Execution, Job
+from prefect_gcp.credentials import GcpCredentials
 
 
 def _get_default_job_body_template() -> Dict[str, Any]:
@@ -30,9 +30,9 @@ def _get_default_job_body_template() -> Dict[str, Any]:
         "metadata": {
             "name": "{{ name }}",
             "annotations": {
-            # See: https://cloud.google.com/run/docs/troubleshooting#launch-stage-validation  # noqa
-            "run.googleapis.com/launch-stage": "BETA",
-            }
+                # See: https://cloud.google.com/run/docs/troubleshooting#launch-stage-validation  # noqa
+                "run.googleapis.com/launch-stage": "BETA",
+            },
         },
         "spec": {  # JobSpec
             "template": {  # ExecutionTemplateSpec
@@ -46,6 +46,7 @@ def _get_default_job_body_template() -> Dict[str, Any]:
                                 }
                             ],
                             "timeoutSeconds": "{{ timeout }}",
+                            # TODO what to do about this?
                             # "serviceAccountName": "{{ service_account_name }}",
                         }  # TaskSpec
                     },
@@ -96,8 +97,8 @@ class CloudRunWorkerJobConfiguration(BaseJobConfiguration):
         """
         Prepares the job configuration for a flow run.
 
-        Ensures that necessary values are present in the job manifest and that the
-        job manifest is valid.
+        Ensures that necessary values are present in the job body and that the
+        job body is valid.
 
         Args:
             flow_run: The flow run to prepare the job configuration for
@@ -108,6 +109,7 @@ class CloudRunWorkerJobConfiguration(BaseJobConfiguration):
         super().prepare_for_flow_run(flow_run, deployment, flow)
         self._populate_envs()
 
+        # TODO what to do about this?
         # set task execution service account to the email of the credentials`
         self.job_body["spec"]["template"]["spec"]["template"]["spec"]["serviceAccountName"] = self.credentials.client_email
 
@@ -117,30 +119,30 @@ class CloudRunWorkerJobConfiguration(BaseJobConfiguration):
 
     def _populate_envs(self):
         envs = [{"name": k, "value": v} for k, v in self.env.items()]
-        self.job_body["spec"]["template"]["spec"]["template"]["spec"]["containers"][0]["env"] = envs
+        self.job_body["spec"]["template"]["spec"]["template"]["spec"]["containers"][0][
+            "env"
+        ] = envs
 
     def _populate_name_if_not_present(self):
         try:
             if "name" not in self.job_body["metadata"]:
                 self.job_body["metadata"]["name"] = f"prefect-job-{uuid4()}"
         except KeyError:
-            raise ValueError(
-                "Unable to verify name due to invalid job body template."
-            )
+            raise ValueError("Unable to verify name due to invalid job body template.")
 
     def _populate_image_if_not_present(self):
         try:
             if (
                 "image"
-                not in self.job_body["spec"]["template"]["spec"]["template"]["spec"]["containers"][0]
+                not in self.job_body["spec"]["template"]["spec"]["template"]["spec"][
+                    "containers"
+                ][0]
             ):
-                self.job_body["spec"]["template"]["spec"]["template"]["spec"]["containers"][0][
-                    "image"
-                ] = f"docker.io/{get_prefect_image_name()}"
+                self.job_body["spec"]["template"]["spec"]["template"]["spec"][
+                    "containers"
+                ][0]["image"] = f"docker.io/{get_prefect_image_name()}"
         except KeyError:
-            raise ValueError(
-                "Unable to verify image due to invalid job body template."
-            )
+            raise ValueError("Unable to verify image due to invalid job body template.")
 
     def _populate_command_if_not_present(self):
         """
@@ -148,15 +150,21 @@ class CloudRunWorkerJobConfiguration(BaseJobConfiguration):
         with the `prefect -m prefect.engine` if a command is not present.
         """
         try:
-            command = self.job_body["spec"]["template"]["spec"]["template"]["spec"]["containers"][0].get("args")
+            command = self.job_body["spec"]["template"]["spec"]["template"]["spec"][
+                "containers"
+            ][0].get("args")
             if command is None:
-                self.job_body["spec"]["template"]["spec"]["template"]["spec"]["containers"][0]["args"] = [
+                self.job_body["spec"]["template"]["spec"]["template"]["spec"][
+                    "containers"
+                ][0]["args"] = [
                     "python",
                     "-m",
                     "prefect.engine",
                 ]
             elif isinstance(command, str):
-                self.job_body["spec"]["template"]["spec"]["template"]["spec"]["containers"][0]["args"] = command.split()
+                self.job_body["spec"]["template"]["spec"]["template"]["spec"][
+                    "containers"
+                ][0]["args"] = command.split()
             elif not isinstance(command, list):
                 raise ValueError(
                     "Invalid job manifest template: 'command' must be a string or list."
@@ -166,50 +174,10 @@ class CloudRunWorkerJobConfiguration(BaseJobConfiguration):
                 "Unable to verify command due to invalid job manifest template."
             )
 
-    def _add_args(self) -> dict:
-        """Set the arguments that will be passed to the entrypoint for a Cloud Run Job.
-        See: https://cloud.google.com/run/docs/reference/rest/v1/Container
-        """  # noqa
-        return {"args": self.args} if self.args else {}
-
-    def _add_command(self) -> dict:
-        """Set the command that a container will run for a Cloud Run Job.
-        See: https://cloud.google.com/run/docs/reference/rest/v1/Container
-        """  # noqa
-        return {"command": self.command}
-
-    def _add_resources(self) -> dict:
-        """Set specified resources limits for a Cloud Run Job.
-        See: https://cloud.google.com/run/docs/reference/rest/v1/Container#ResourceRequirements
-        See also: https://kubernetes.io/docs/concepts/configuration/manage-resources-containers/
-        """  # noqa
-        resources = {"limits": {}, "requests": {}}
-
-        if self.cpu is not None:
-            cpu = self._cpu_as_k8s_quantity()
-            resources["limits"]["cpu"] = cpu
-            resources["requests"]["cpu"] = cpu
-        if self.memory_string is not None:
-            resources["limits"]["memory"] = self.memory_string
-            resources["requests"]["memory"] = self.memory_string
-
-        return {"resources": resources} if resources["requests"] else {}
-
-    def _add_env(self) -> dict:
-        """Add environment variables for a Cloud Run Job.
-
-        Method `self._base_environment()` gets necessary Prefect environment variables
-        from the config.
-
-        See: https://cloud.google.com/run/docs/reference/rest/v1/Container#envvar for
-        how environment variables are specified for Cloud Run Jobs.
-        """  # noqa
-        env = {**self._base_environment(), **self.env}
-        cloud_run_env = [{"name": k, "value": v} for k, v in env.items()]
-        return {"env": cloud_run_env}
-
 
 class CloudRunWorkerVariables(BaseVariables):
+    region: str = Field(..., description="The region where the Cloud Run Job resides.")
+    credentials: GcpCredentials  # cannot be Field; else it shows as Json
     image: Optional[str] = Field(
         default=None,
         title="Image Name",
@@ -218,38 +186,6 @@ class CloudRunWorkerVariables(BaseVariables):
             "for supported image registries. If not set, the latest Prefect image will be used."
         ),
         example="docker.io/prefecthq/prefect:2-latest",
-    )
-    region: str = Field(..., description="The region where the Cloud Run Job resides.")
-    credentials: GcpCredentials  # cannot be Field; else it shows as Json
-
-    # Job settings
-    cpu: Optional[int] = Field(
-        default=None,
-        title="CPU",
-        description=(
-            "The amount of compute allocated to the Cloud Run Job. "
-            "The int must be valid based on the rules specified at "
-            "https://cloud.google.com/run/docs/configuring/cpu#setting-jobs ."
-        ),
-    )
-    memory: Optional[int] = Field(
-        default=None,
-        title="Memory",
-        description="The amount of memory allocated to the Cloud Run Job.",
-    )
-    memory_unit: Optional[Literal["G", "Gi", "M", "Mi"]] = Field(
-        default=None,
-        title="Memory Units",
-        description=(
-            "The unit of memory. See "
-            "https://cloud.google.com/run/docs/configuring/memory-limits#setting "
-            "for additional details."
-        ),
-    )
-    vpc_connector_name: Optional[str] = Field(
-        default=None,
-        title="VPC Connector Name",
-        description="The name of the VPC connector to use for the Cloud Run Job.",
     )
     args: Optional[List[str]] = Field(
         default=None,
@@ -283,35 +219,9 @@ class CloudRunWorker(BaseWorker):
     job_configuration = CloudRunWorkerJobConfiguration
     job_configuration_variables = CloudRunWorkerVariables
 
-    async def _check_flow_run(self, flow_run: "FlowRun") -> None:
-        pass
-
-    @property
-    def memory_string(self):
-        """Returns the string expected for memory resources argument."""
-        if self.memory and self.memory_unit:
-            return str(self.memory) + self.memory_unit
-        return None
-
-    @validator("image")
-    def _remove_image_spaces(cls, value):
-        """Deal with spaces in image names."""
-        if value is not None:
-            return value.strip()
-
-    @root_validator
-    def _check_valid_memory(cls, values):
-        """Make sure memory conforms to expected values for API.
-        See: https://cloud.google.com/run/docs/configuring/memory-limits#setting
-        """  # noqa
-        if (values.get("memory") is not None and values.get("memory_unit") is None) or (
-            values.get("memory_unit") is not None and values.get("memory") is None
-        ):
-            raise ValueError(
-                "A memory value and unit must both be supplied to specify a memory"
-                " value other than the default memory value."
-            )
-        return values
+    # TODO: remove
+    # async def _check_flow_run(self, flow_run: "FlowRun") -> None:
+    #     pass
 
     def _create_job_error(self, exc, configuration):
         """Provides a nicer error for 404s when trying to create a Cloud Run Job."""
@@ -350,13 +260,6 @@ class CloudRunWorker(BaseWorker):
 
         raise exc
 
-    def _cpu_as_k8s_quantity(self) -> str:
-        """Return the CPU integer in the format expected by GCP Cloud Run Jobs API.
-        See: https://kubernetes.io/docs/concepts/configuration/manage-resources-containers/
-        See also: https://cloud.google.com/run/docs/configuring/cpu#setting-jobs
-        """  # noqa
-        return str(self.cpu * 1000) + "m"
-
     @sync_compatible
     async def run(
         self,
@@ -364,7 +267,6 @@ class CloudRunWorker(BaseWorker):
         configuration: CloudRunWorkerJobConfiguration,
         task_status: Optional[anyio.abc.TaskStatus] = None,
     ) -> BaseWorkerResult:
-
         with self._get_client(configuration) as client:
             await run_sync_in_worker_thread(
                 self._create_job_and_wait_for_registration, configuration, client
@@ -395,7 +297,9 @@ class CloudRunWorker(BaseWorker):
             "run", "v1", client_options=options, credentials=gcp_creds
         ).namespaces()
 
-    def _create_job_and_wait_for_registration(self, configuration: CloudRunWorkerJobConfiguration, client: Resource) -> None:
+    def _create_job_and_wait_for_registration(
+        self, configuration: CloudRunWorkerJobConfiguration, client: Resource
+    ) -> None:
         """Create a new job wait for it to finish registering."""
         try:
             self._logger.info(f"Creating Cloud Run Job {configuration.job_name}")
@@ -431,7 +335,9 @@ class CloudRunWorker(BaseWorker):
                     )
             raise
 
-    def _begin_job_execution(self, configuration: CloudRunWorkerJobConfiguration, client: Resource) -> Execution:
+    def _begin_job_execution(
+        self, configuration: CloudRunWorkerJobConfiguration, client: Resource
+    ) -> Execution:
         """Submit a job run for execution and return the execution object."""
         try:
             self._logger.info(
@@ -454,7 +360,11 @@ class CloudRunWorker(BaseWorker):
         return job_execution
 
     def _watch_job_execution_and_get_result(
-        self, configuration: CloudRunWorkerJobConfiguration, client: Resource, execution: Execution, poll_interval: int=5
+        self,
+        configuration: CloudRunWorkerJobConfiguration,
+        client: Resource,
+        execution: Execution,
+        poll_interval: int = 5,
     ) -> CloudRunWorkerResult:
         """Wait for execution to complete and then return result."""
         try:
@@ -473,7 +383,9 @@ class CloudRunWorker(BaseWorker):
 
         if job_execution.succeeded():
             status_code = 0
-            self._logger.info(f"Job Run {configuration.job_name} completed successfully")
+            self._logger.info(
+                f"Job Run {configuration.job_name} completed successfully"
+            )
         else:
             status_code = 1
             error_msg = job_execution.condition_after_completion()["message"]
@@ -502,7 +414,9 @@ class CloudRunWorker(BaseWorker):
                     f" Run Job {configuration.job_name}"
                 )
 
-        return CloudRunWorkerResult(identifier=configuration.job_name, status_code=status_code)
+        return CloudRunWorkerResult(
+            identifier=configuration.job_name, status_code=status_code
+        )
 
     def _watch_job_execution(
         self, client, job_execution: Execution, timeout: int, poll_interval: int = 5
@@ -530,11 +444,16 @@ class CloudRunWorker(BaseWorker):
         return job_execution
 
     def _wait_for_job_creation(
-        self, client: Resource, configuration: CloudRunWorkerJobConfiguration, poll_interval: int = 5
+        self,
+        client: Resource,
+        configuration: CloudRunWorkerJobConfiguration,
+        poll_interval: int = 5,
     ):
         """Give created job time to register."""
         job = Job.get(
-            client=client, namespace=configuration.project, job_name=configuration.job_name
+            client=client,
+            namespace=configuration.project,
+            job_name=configuration.job_name,
         )
 
         t0 = time.time()
@@ -554,7 +473,10 @@ class CloudRunWorker(BaseWorker):
             )
 
             elapsed_time = time.time() - t0
-            if configuration.timeout is not None and elapsed_time > configuration.timeout:
+            if (
+                configuration.timeout is not None
+                and elapsed_time > configuration.timeout
+            ):
                 raise RuntimeError(
                     f"Timed out after {elapsed_time}s while waiting for Cloud Run Job "
                     "execution to complete. Your job may still be running on GCP."
