faad412cc16bd2d6ad5c0a14ff4d03919427a103
Alexander Streed <desertaxle@users.noreply.github.com>
2023-05-16 08:44:43 -0500

    Satisfy flake8

--- prefect_gcp/projects/steps.py
+++ prefect_gcp/projects/steps.py
@@ -44,8 +44,8 @@ def push_project_to_gcs(
     Args:
         bucket: The name of the GCS bucket where the project files will be uploaded.
         folder: The folder in the GCS bucket where the project files will be uploaded.
-        project: The GCP project the bucket belongs to. If not provided, the project will be
-            inferred from the credentials or the local environment.
+        project: The GCP project the bucket belongs to. If not provided, the project 
+            will be inferred from the credentials or the local environment.
         credentials: A dictionary containing the service account information and project
             used for authentication. If not provided, the application default
             credentials will be used.
