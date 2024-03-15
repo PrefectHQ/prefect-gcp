845e3970cc37eb4a205de2926066023453ff3116
Alex Streed <alex.s@prefect.io>
2024-03-15 08:34:30 -0500

    Fixes tests

--- tests/test_deprecation.py
+++ tests/test_deprecation.py
@@ -6,24 +6,31 @@ from prefect_gcp.cloud_run import CloudRunJob
 
 
 @pytest.mark.parametrize(
-    "InfraBlock, expected_message",
+    "InfraBlock, kwargs, expected_message",
     [
         (
             CloudRunJob,
+            {"image": "foo", "region": "us-central1"},
             "prefect_gcp.cloud_run.CloudRunJob has been deprecated."
-            " It will not be available after Sep 2024."
-            " Use the Vertex AI worker instead."
-            " Refer to the upgrade guide for more information",
-        ),
-        (
-            VertexAICustomTrainingJob,
-            "prefect_gcp.aiplaform.VertexAICustomTrainingJob has been deprecated."
             " It will not be available after Sep 2024."
             " Use the Cloud Run or Cloud Run v2 worker instead."
             " Refer to the upgrade guide for more information",
         ),
+        (
+            VertexAICustomTrainingJob,
+            {"image": "foo", "region": "us-central1"},
+            "prefect_gcp.aiplatform.VertexAICustomTrainingJob has been deprecated."
+            " It will not be available after Sep 2024."
+            " Use the Vertex AI worker instead."
+            " Refer to the upgrade guide for more information",
+        ),
     ],
 )
-def test_infra_blocks_emit_a_deprecation_warning(InfraBlock, expected_message):
+def test_infra_blocks_emit_a_deprecation_warning(
+    InfraBlock, kwargs, expected_message, gcp_credentials
+):
     with pytest.warns(PrefectDeprecationWarning, match=expected_message):
-        InfraBlock()
+        if InfraBlock == CloudRunJob:
+            InfraBlock(**kwargs, credentials=gcp_credentials)
+        else:
+            InfraBlock(**kwargs, gcp_credentials=gcp_credentials)
